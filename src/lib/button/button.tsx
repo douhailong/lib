e5c81afa9c0de3af79e9e40925040a09d6aa5d37
douhailong <hailong.dou@d3j.io>
2023-02-20 12:33:37 +0800

    init

---
+++ src/lib/button/button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IButtonProps {}
+
+const Button: React.FC<IButtonProps> = (props: IButtonProps) => {
+    return <div>Button</div>;
+};
+
+export default Button;
+
+
